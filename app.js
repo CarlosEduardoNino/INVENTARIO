db990e0f1ac177d24d7d1e0fd5d30c1b3a521887
CarlosEduardoNino <carlos.eduardo.ninoferreira@gmail.com>
2024-11-26 09:38:32 -0500

    Arreglos simples

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'));
 app.use('/api/Articulos', articulos);
 app.use('/api/Categorias', categorias);
 app.use('/api/Movimientos', movimientos);
